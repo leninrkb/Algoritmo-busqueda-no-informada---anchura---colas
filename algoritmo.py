85a0508a3c6ce50ed336d7f704476b587f17cd98
Lenin Acosta <rkb.lenin@gmail.com>
2023-04-06 11:20:54 -0500

    docs y metricas de tiempos y nodos generados

--- algoritmo.py
+++ algoritmo.py
@@ -7,17 +7,29 @@ def generar_estados_posibles(laberinto, agente):
     y retorna una lista con los que se lograron generar. 
     cada agente guarda su gente padre'''
 
-    lista = []
+    # conpruebo si el agente ya corresponde a mi
+    # funcion objetivo
     if agente.es_funcion_objetivo():
         imprimir_ruta(agente)
         return True
+    
+    # lista a retornar con los nuevos estados
+    lista = []
+    
+    # compruebo si ela gente aparecio en un lugar psible en el
+    # laberinto
     if laberinto.laberinto[agente.posx][agente.posy] == 1:
+        # genera una copia del padre para generar los estados
         agente_arriba = copy.copy(agente)
         agente_abajo = copy.copy(agente)
         agente_izquierda = copy.copy(agente)
         agente_derecha = copy.copy(agente)
 
+        # compruebo si el movimiento a realizar esta dentro del laberinto
         if agente_arriba.arriba():
+
+            # comrpuebo si el movimiento es posible dentro del camino
+            # y no es un muro
             if laberinto.laberinto[agente_arriba.posx][agente_arriba.posy] == 1:
                 print(agente_arriba.posx, agente_arriba.posy)
                 agente_arriba.padre = agente
@@ -59,25 +71,38 @@ def generar_estados_posibles(laberinto, agente):
         print('\n')
         return lista
     else:
-        print('el agente spawneo en un lugar imposible')
         return None
 
 
 def agregar_agentes_cola(cola, lista_agentes, trabajados):
+    ''' agrega una lista de agentes a la cola '''
     for agente in lista_agentes:
         if es_repetido(agente, trabajados):
             continue
         cola.put(agente)
 
 def es_repetido(agente, trabajados):
+    ''' verifica si la posicion del agente 
+     ya existio '''
     for aux_agente in trabajados:
         if aux_agente.posx == agente.posx and aux_agente.posy == agente.posy:
             return True
     return False
 
-
 def imprimir_ruta(agente):
+    ''' imprime la ruta del agente que llego al objetivo '''
     if agente == None:
         return
     print(f'posicion x: {agente.posx} - posicion y: {agente.posy}')
     imprimir_ruta(agente.padre)
+
+def control_flujo(resp, cola, trabajados):
+    if resp == True:
+        print('--- solucion encontrada ---')
+        return True
+    elif not resp == None:
+        agregar_agentes_cola(cola,resp, trabajados)
+        return False
+    else:
+        print('spawneado imposible')
+        return False
--- main.py
+++ main.py
@@ -1,40 +1,64 @@
 # from numba import njit
-import pprint
 import queue
 from laberinto import Laberinto
 from agente import Agente
 import algoritmo
 import copy
+import time
 
 laberinto = Laberinto()
 agente = Agente()
 cola = queue.Queue()
 
-agente.maxx, agente.maxy = laberinto.laberinto.shape
+# obtengo la forma de la matriz laberinto
+# para controlar que el agente no se salga de los limites
+agente.maxx, agente.maxy = laberinto.laberinto.shape 
+
+# obtengo las coordenadas que corresponden a la salida
 agente.xob, agente.yob = laberinto.fin
 
+#lista para guardar los estados que ya generaron hijos y evitar ciclos
+trabajados = [] 
 
+total_nodos = 1
+
+# genero los primeros estados a partir del nodo inicial
 resp = algoritmo.generar_estados_posibles(laberinto, agente)
-trabajados = []
+if isinstance(resp, list): 
+    total_nodos += len(resp)
 
-if resp == True:
-    print('se hallo solucion')
-elif not resp == None:
-    algoritmo.agregar_agentes_cola(cola,resp, trabajados)
-else:
-    print('spawneado imposible')
+algoritmo.control_flujo(resp, cola, trabajados)
 
-
+total = 0
 while not cola.empty():
-    agente = cola.get()
+    inicio = time.time()
+
+    # obtiene el agente y lo elimina de la cola
+    agente = cola.get() 
+
+    # registro el agente en la lista de trabajados para no volver 
+    # a tomarlos en cuenta
     trabajados.append(copy.copy(agente))
+
+    # genero los estados posibles del agente
     resp = algoritmo.generar_estados_posibles(laberinto, agente)
-    if resp == True:
-        print('se hallo solucion')
+    if isinstance(resp, list): 
+        total_nodos += len(resp)
+
+
+    # controlo las acciones posteriores
+    if algoritmo.control_flujo(resp, cola, trabajados):
         break
-    elif not resp == None:
-        algoritmo.agregar_agentes_cola(cola,resp,trabajados)
-    else:
-        print('spawneado imposible')
-        break
+    
+    fin = time.time()
+    sub_total = fin - inicio
+    total += sub_total
+    print(f'tiempo por una ejecucion completa del agente: {sub_total} segundos')
+
+print(f'tiempo total de la ejecucion: {total} segundos')
+print(f'nodos generados: {total_nodos}')
+
+
+
+    
 
